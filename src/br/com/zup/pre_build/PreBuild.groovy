344b96edfbdda517305c80e0729fdc16e452f55a
Vinicius Sousa <csousa.vinicius@gmail.com>
2021-03-09 14:59:57 -0300

    [Flutter] new: pre-build step

---
+++ src/br/com/zup/pre_build/PreBuild.groovy
@@ -0,0 +1,28 @@
+package br.com.zup.pre_build
+
+class PreBuild {
+    def call (jenkins) {
+        jenkins.podTemplate(
+            containers: [
+                jenkins.containerTemplate(
+                    name: 'flutter', 
+                    image: 'flutter', 
+                    ttyEnabled: true, 
+                    command: 'cat'
+                )
+            ],
+            yamlMergeStrategy: jenkins.merge(),
+            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                readOnly: false
+            )
+        ) {
+            jenkins.node(jenkins.POD_LABEL){
+                jenkins.container('gradle'){
+                    jenkins.sh label: "Pre-Build flutter", 
+                               script: "flutter doctor -v"
+                }
+            }
+        }
+    }
+}
